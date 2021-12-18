3f3fbfce3b38de33fab873f9a06779273917200f
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-12-18 17:16:25 +0200

    fix form

--- src/components/Forma/Forma.js
+++ src/components/Forma/Forma.js
@@ -1,61 +1,74 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import css from "./Forma.module.css";
 
-class Form extends Component {
-  state = {
-    name: "",
-    number: "",
-  };
-  handleInputChange = (e) => {
+const Form = ({ onSubmit }) => {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+
+  const handleInputChange = (e) => {
     const { name, value } = e.currentTarget;
 
-    this.setState({
-      // [e.currentTarget.name]: e.currentTarget.number,
-      [name]: value,
-    });
-  };
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "number":
+        setNumber(value);
+        break;
 
-  handleSubmit = (e) => {
+      // switch (e.currentTarget.name) {
+      //   case "e.currentTarget.name":
+      //     setName(e.currentTarget.value);
+      //     break;
+      //   case "e.currentTarget.number":
+      //     setNumber(e.currentTarget.value);
+      //     break;
+      //   // [e.currentTarget.name]: e.currentTarget.number,
+      //   // [name]: value,
+      default:
+        return;
+    }
+  };
+  const handleSubmit = (e) => {
     e.preventDefault();
     // console.log(this.state);
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit(name, number);
+    reset();
   };
 
-  reset = () => {
-    this.setState({ name: "", number: "" });
+  const reset = () => {
+    setName("");
+    setNumber("");
   };
 
-  render() {
-    return (
-      <form className={css.form} onSubmit={this.handleSubmit}>
-        <label className={css.label}>
-          Name
-          <input
-            type="text"
-            name="name"
-            value={this.state.name}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <label className={css.label}>
-          Number
-          <input
-            type="tel"
-            name="number"
-            value={this.state.number}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <button className={css.btn}>Add contact</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <label className={css.label}>
+        Name
+        <input
+          type="text"
+          name="name"
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={handleInputChange}
+        />
+      </label>
+      <label className={css.label}>
+        Number
+        <input
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={handleInputChange}
+        />
+      </label>
+      <button className={css.btn}>Add contact</button>
+    </form>
+  );
+};
 export default Form;
